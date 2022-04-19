18621cd736d6f28681de72a02e5989d0836155d6
Oscar H.V <55415043+OscarViquez@users.noreply.github.com>
2022-04-18 21:40:35 -0400

    Made code a bit more organized

--- js/5-very-hard.js
+++ js/5-very-hard.js
@@ -1,40 +1,32 @@
 'use strict';
-
 const firstNumber = prompt('Welcome to the Calculator, enter a number you want to use');
 const operator = prompt('Choose either +, -, /, or *');
 const secondNumber = prompt("Enter a second number you want to use");
 
 function add() {
-    console.log(Number(firstNumber) +  Number(secondNumber) )
+    console.log(`${Number(firstNumber)} + ${Number(secondNumber)} = ${Number(firstNumber) +  Number(secondNumber)}`)
 }
-
 function subtract() {
-    console.log(Number(firstNumber) -  Number(secondNumber) )
+    console.log(`${Number(firstNumber)} - ${Number(secondNumber)} = ${Number(firstNumber) -  Number(secondNumber)}`)
 }
-
 function divide() {
-    console.log(Number(firstNumber) /  Number(secondNumber) )
+    console.log(`${Number(firstNumber)} / ${Number(secondNumber)} = ${Number(firstNumber) /  Number(secondNumber)}`)
 }
-
 function multiply() {
-    console.log(Number(firstNumber) *  Number(secondNumber) )
+    console.log(`${Number(firstNumber)} * ${Number(secondNumber)} = ${Number(firstNumber) *  Number(secondNumber)}`)
 }
 
 function Calculator() {
     if (operator === "+") {
         add()
-
     } else if (operator === "-") {
         subtract()
-
     } else if (operator === "/") {
         divide()
-        
     } else if (operator === "*") {
         multiply()
     } else {
         console.error('calculator ERROR')
     }
 }
-
 Calculator()
