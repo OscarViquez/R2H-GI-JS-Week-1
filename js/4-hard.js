f3f7ec843e0e152e4956ca910ff1821b536938c0
Oscar H.V <55415043+OscarViquez@users.noreply.github.com>
2022-04-18 21:16:19 -0400

    Completed challenges

---
+++ js/4-hard.js
@@ -0,0 +1,23 @@
+'use strict';
+let a = 8;
+let b = 2;
+
+function add() {
+    return a + b
+}
+console.log(add())
+
+function subtract() {
+    return a - b
+}
+console.log(subtract())
+
+function multiply() {
+    return a * b
+}
+console.log(multiply())
+
+function divide() {
+    return a / b
+}
+console.log(divide())
